b60720d58cb4e9061c2ee860f2c18ac669e166b4
carlosQuin964 <caquintero94@misena.edu.co>
2016-08-27 05:32:07 -0500

    se agrego la paguina de inicio

--- OrfiPlusWeb/build/generated-sources/ap-source-output/com/orfi/entity/AuditoriaRegistro_.java
+++ OrfiPlusWeb/build/generated-sources/ap-source-output/com/orfi/entity/AuditoriaRegistro_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-25T21:22:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-27T04:48:50")
 @StaticMetamodel(AuditoriaRegistro.class)
 public class AuditoriaRegistro_ { 
 
--- OrfiPlusWeb/build/generated-sources/ap-source-output/com/orfi/entity/Persona_.java
+++ OrfiPlusWeb/build/generated-sources/ap-source-output/com/orfi/entity/Persona_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-25T21:22:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-27T04:48:50")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
