e247f43ebc5e0d15bef37d2b9b75c01f8a51a0f1
LilyRozsak <lmrosales0@misena.edu.co>
2016-11-01 14:19:22 -0500

    clase correos
    la lógica donde para el envió de correos

---
+++ OrfiPlusWeb/src/java/Correos/correoTexto.java
@@ -0,0 +1,87 @@
+package Correos;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+
+
+
+public class correoTexto {
+    
+    
+
+    public void mandarCorreo() {
+        // El correo gmail de envío
+        String correoEnvia = "liliana.rosales.ga@gmail.com";
+        String claveCorreo = "2021rosales";
+
+        // La configuración para enviar correo
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.user", correoEnvia);
+        properties.put("mail.password", claveCorreo);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+        // Obtener la sesion
+        Session session = Session.getInstance(properties, null);
+
+        try {
+            // Crear el cuerpo del mensaje
+            MimeMessage mimeMessage = new MimeMessage(session);
+
+            // Agregar quien envía el correo
+            mimeMessage.setFrom(new InternetAddress(correoEnvia, "OrfiPlus"));
+
+            // Los destinatarios
+            InternetAddress[] internetAddresses = {
+                new InternetAddress("lmrosales0@misena.edu.co"),
+                new InternetAddress("liliana.princess16@hotmail.com"),
+                new InternetAddress("chris930211@hotmail.com")};
+
+            // Agregar los destinatarios al mensaje
+            mimeMessage.setRecipients(Message.RecipientType.TO,
+                    internetAddresses);
+
+            // Agregar el asunto al correo
+            mimeMessage.setSubject("OrfiPlus.com");
+
+            // Creo la parte del mensaje
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setText("El mensaje que se desea");
+
+            // Crear el multipart para agregar la parte del mensaje anterior
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            // Agregar el multipart al cuerpo del mensaje
+            mimeMessage.setContent(multipart);
+
+            // Enviar el mensaje
+            Transport transport = session.getTransport("smtp");
+            transport.connect(correoEnvia, claveCorreo);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Correo enviado");
+    }
+
+    public static void main(String[] args) {
+        correoTexto correoTexto = new correoTexto();
+        correoTexto.mandarCorreo();
+
+    }
+    
+}
